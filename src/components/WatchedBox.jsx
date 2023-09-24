4dc15c306cef8f978a426a0db0e6ba7feb0f5310
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-24 18:21:39 +0330

    Add WatchedList component

--- src/components/WatchedBox.jsx
+++ src/components/WatchedBox.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import WatchedSummary from './WatchedSummary';
+import WatchedList from './WatchedList';
 
 const tempWatchedData = [
     {
@@ -37,31 +38,7 @@ const WatchedBox = () => {
             {isOpen2 && (
                 <>
                     <WatchedSummary watched={watched} />
-                    <ul className='list'>
-                        {watched.map((movie) => (
-                            <li key={movie.imdbID}>
-                                <img
-                                    src={movie.Poster}
-                                    alt={`${movie.Title} poster`}
-                                />
-                                <h3>{movie.Title}</h3>
-                                <div>
-                                    <p>
-                                        <span>⭐️</span>
-                                        <span>{movie.imdbRating}</span>
-                                    </p>
-                                    <p>
-                                        <span>🌟</span>
-                                        <span>{movie.userRating}</span>
-                                    </p>
-                                    <p>
-                                        <span>⏳</span>
-                                        <span>{movie.runtime} min</span>
-                                    </p>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                    <WatchedList watched={watched} />
                 </>
             )}
         </div>
