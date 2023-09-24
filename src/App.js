4d6ecfb9dadc8c2cafbc4bd627c747aa45c2cafb
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-24 17:38:28 +0330

    Add main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Main from './components/Main';
 import NavBar from './components/NavBar';
 
 function App() {
     return (
         <>
             <NavBar />
+            <Main />
         </>
     );
 }
