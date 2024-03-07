b02a2635df55865271ca91cc7e3d5f67055ad669
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-07 20:57:04 +0330

    Using Composition to Make a Reusable Box

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import NavBar from './components/NavBar';
 import { useState } from 'react';
 import Search from './components/Search';
 import NumResults from './components/NumResults';
-import ListBox from './components/ListBox';
-import WatchedBox from './components/WatchedBox';
+import Box from './components/Box';
 import MovieList from './components/MovieList';
+import WatchedSummary from './components/WatchedSummary';
+import WatchedList from './components/WatchedList';
 
 const tempMovieData = [
  {
@@ -31,8 +32,33 @@ const tempMovieData = [
  },
 ];
 
+const tempWatchedData = [
+ {
+  imdbID: 'tt1375666',
+  Title: 'Inception',
+  Year: '2010',
+  Poster:
+   'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+  runtime: 148,
+  imdbRating: 8.8,
+  userRating: 10,
+ },
+ {
+  imdbID: 'tt0088763',
+  Title: 'Back to the Future',
+  Year: '1985',
+  Poster:
+   'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+  runtime: 116,
+  imdbRating: 8.5,
+  userRating: 9,
+ },
+];
+
 function App() {
  const [movies, setMovies] = useState(tempMovieData);
+ const [watched, setWatched] = useState(tempWatchedData);
+
  return (
   <>
    <NavBar>
@@ -40,10 +66,13 @@ function App() {
     <NumResults movies={movies} />
    </NavBar>
    <Main>
-    <ListBox>
+    <Box>
      <MovieList movies={movies} />
-    </ListBox>
-    <WatchedBox />
+    </Box>
+    <Box>
+     <WatchedSummary watched={watched} />
+     <WatchedList watched={watched} />
+    </Box>
    </Main>
   </>
  );
