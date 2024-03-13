9a941fa3a89f3f8bc221dcfe656592405c203eea
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-13 23:20:05 +0330

    Improving reusablility with props

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -11,17 +11,26 @@ const starContainerStyle = {
  display: 'flex',
 };
 
-const textStyle = {
- lineHeight: '1',
- margin: '0',
-};
-
-const StarRating = ({ maxRating = 10 }) => {
- const [rating, setRating] = useState(0);
+const StarRating = ({
+ maxRating = 10,
+ color = '#fcc419',
+ size = 48,
+ className = '',
+ messages = [],
+ defaultRating = 0,
+}) => {
+ const [rating, setRating] = useState(defaultRating);
  const [tempRating, setTempRating] = useState(0);
 
+ const textStyle = {
+  lineHeight: '1',
+  margin: '0',
+  color,
+  fontSize: `${size / 1.5}px`,
+ };
+
  return (
-  <div style={containerStyles}>
+  <div style={containerStyles} className={className}>
    <div style={starContainerStyle}>
     {Array.from({ length: maxRating }, (_, i) => (
      <Star
@@ -30,10 +39,16 @@ const StarRating = ({ maxRating = 10 }) => {
       onRate={() => setRating(i + 1)}
       onHoverIn={() => setTempRating(i + 1)}
       onHoverOut={() => setTempRating(0)}
+      color={color}
+      size={size}
      />
     ))}
    </div>
-   <p style={textStyle}>{tempRating || rating || ''}</p>
+   <p style={textStyle}>
+    {messages.length === maxRating
+     ? messages[tempRating ? tempRating - 1 : rating - 1]
+     : tempRating || rating || ''}
+   </p>
   </div>
  );
 };
