73fbbcb8723c5d4048f2bc17e26a4c79c455bfdf
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-21 15:00:19 +0330

    Handle set rating

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -34,6 +34,11 @@ export default function StarRating({
  const [rating, setRating] = useState(defaultRating);
  const [tempRating, setTempRating] = useState(0);
 
+ const handleSetRating = (rating) => {
+  setRating(rating);
+  onSetRating(rating);
+ };
+
  const textStyle = {
   lineHeight: '1',
   margin: '0',
@@ -48,7 +53,7 @@ export default function StarRating({
      <Star
       key={i}
       full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-      onRate={() => setRating(i + 1)}
+      onRate={() => handleSetRating(i + 1)}
       onHoverIn={() => setTempRating(i + 1)}
       onHoverOut={() => setTempRating(0)}
       color={color}
