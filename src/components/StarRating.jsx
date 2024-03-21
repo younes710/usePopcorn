1197ad554483d6a59038d6d9688588760fe36c39
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-21 13:44:36 +0330

    Prop types

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Star from './Star';
+import PropTypes from 'prop-types';
 
 const containerStyles = {
  display: 'flex',
@@ -11,14 +12,25 @@ const starContainerStyle = {
  display: 'flex',
 };
 
-const StarRating = ({
+StarRating.propTypes = {
+ maxRating: PropTypes.number,
+ defaultRating: PropTypes.number,
+ color: PropTypes.string.isRequired,
+ size: PropTypes.number,
+ className: PropTypes.string,
+ messages: PropTypes.array,
+ onSetRating: PropTypes.func,
+};
+
+export default function StarRating({
  maxRating = 10,
  color = '#fcc419',
  size = 48,
  className = '',
  messages = [],
  defaultRating = 0,
-}) => {
+ onSetRating,
+}) {
  const [rating, setRating] = useState(defaultRating);
  const [tempRating, setTempRating] = useState(0);
 
@@ -51,6 +63,4 @@ const StarRating = ({
    </p>
   </div>
  );
-};
-
-export default StarRating;
+}
