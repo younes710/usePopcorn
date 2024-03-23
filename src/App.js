477b0ca4e6050e83a00c990204babff8a83ddc54
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-23 11:08:39 +0330

    Fix styles and use useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Main from './components/Main';
 import NavBar from './components/NavBar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Search from './components/Search';
 import NumResults from './components/NumResults';
 import Box from './components/Box';
@@ -8,56 +8,17 @@ import MovieList from './components/MovieList';
 import WatchedSummary from './components/WatchedSummary';
 import WatchedList from './components/WatchedList';
 
-const tempMovieData = [
- {
-  imdbID: 'tt1375666',
-  Title: 'Inception',
-  Year: '2010',
-  Poster:
-   'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
- },
- {
-  imdbID: 'tt0133093',
-  Title: 'The Matrix',
-  Year: '1999',
-  Poster:
-   'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
- },
- {
-  imdbID: 'tt6751668',
-  Title: 'Parasite',
-  Year: '2019',
-  Poster:
-   'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
- },
-];
-
-const tempWatchedData = [
- {
-  imdbID: 'tt1375666',
-  Title: 'Inception',
-  Year: '2010',
-  Poster:
-   'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  runtime: 148,
-  imdbRating: 8.8,
-  userRating: 10,
- },
- {
-  imdbID: 'tt0088763',
-  Title: 'Back to the Future',
-  Year: '1985',
-  Poster:
-   'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-  runtime: 116,
-  imdbRating: 8.5,
-  userRating: 9,
- },
-];
+const KEY = 'f84fc31d';
 
 function App() {
- const [movies, setMovies] = useState(tempMovieData);
- const [watched, setWatched] = useState(tempWatchedData);
+ const [movies, setMovies] = useState([]);
+ const [watched, setWatched] = useState([]);
+
+ useEffect(() => {
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+   .then((res) => res.json())
+   .then((data) => setMovies(data.Search));
+ }, []);
 
  return (
   <>
