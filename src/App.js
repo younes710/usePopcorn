e4cbe56ea859b43c9678c4ffa2838b9250ab34a4
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-23 12:18:07 +0330

    Adding a loading state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Box from './components/Box';
 import MovieList from './components/MovieList';
 import WatchedSummary from './components/WatchedSummary';
 import WatchedList from './components/WatchedList';
+import Loader from './components/Loader';
 
 const KEY = 'f84fc31d';
 const query = 'interstellar';
@@ -14,12 +15,15 @@ const query = 'interstellar';
 function App() {
  const [movies, setMovies] = useState([]);
  const [watched, setWatched] = useState([]);
+ const [isLoading, setIsLoading] = useState(false);
 
  useEffect(() => {
   const fetchMovies = async () => {
+   setIsLoading(true);
    const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
    const data = await res.json();
    setMovies(data.Search);
+   setIsLoading(false);
   };
   fetchMovies();
  }, []);
@@ -31,7 +35,7 @@ function App() {
     <NumResults movies={movies} />
    </NavBar>
    <Main>
-    <Box element={<MovieList movies={movies} />} />
+    <Box element={isLoading ? <Loader /> : <MovieList movies={movies} />} />
     <Box
      element={
       <>
